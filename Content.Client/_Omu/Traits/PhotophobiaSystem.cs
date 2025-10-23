fd7c40c25ebc19b31128a66f2072a0a603c6b3cd
Gip-Kip <hargifarspam2@gmail.com>
2025-10-23 22:07:02 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Omu/Traits/PhotophobiaSystem.cs
+++ Content.Client/_Omu/Traits/PhotophobiaSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 MarkerWicker <markerWicker@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client._Omu.Eye;
 using Content.Shared._Omu.Traits;
 using Content.Shared.Flash.Components;
